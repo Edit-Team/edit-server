3af94212efea3642077518d3fb66759089f6a91a
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-05 03:10:18 +0900

    refactor : 임시 자소서 Entity에 누락된 주석 추가

--- src/main/java/com/app/edit/domain/temporarycoverletter/TemporaryCoverLetter.java
+++ src/main/java/com/app/edit/domain/temporarycoverletter/TemporaryCoverLetter.java
@@ -18,24 +18,44 @@ import javax.persistence.*;
 @Table(name = "TEMPORARY_COVER_LETTER")
 public class TemporaryCoverLetter extends BaseEntity {
 
+    /*
+     * 임시 자소서 ID
+     **/
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
+    /*
+     * 자소서 카테고리 ID
+     **/
     @Column(name = "categoryId", nullable = false, updatable = false)
     private Long categoryId;
 
+    /*
+     * 임시 자소서 작성 유저 ID
+     **/
     @Column(name = "userInfoId", nullable = false, updatable = false)
     private Long userInfoId;
 
+    /*
+     * 임시 자소서 내용
+     **/
     @Column(name = "content", nullable = false, length = 90)
     private String content;
 
+    /*
+     * 임시 자소서 타입
+     * 작성 중인 자소서 - WRITING, 완성 중인 자소서 - COMPLETING
+     **/
     @Enumerated(EnumType.STRING)
     @Column(name = "type", nullable = false, length = 15)
     private CoverLetterType type;
 
+    /*
+     * 자소서 삭제 여부
+     * ACTIVE - 삭제되지 않음, INACTIVE - 삭제됨
+     **/
     @Enumerated(EnumType.STRING)
     @Column(name = "state", nullable = false, columnDefinition = "varchar(10) default 'ACTIVE'")
     private State state;
