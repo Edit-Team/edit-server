279f10d92036d3eaad4d891215126eeecbdf6bf7
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-09 04:13:59 +0900

    style : 불필요한 import문 제거

--- src/main/java/com/app/edit/domain/coverletter/CoverLetterRepository.java
+++ src/main/java/com/app/edit/domain/coverletter/CoverLetterRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface CoverLetterRepository extends JpaRepository<CoverLetter, Long> {
