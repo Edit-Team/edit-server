793127fdffe87d690307a894119e816510137957
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-08 18:39:10 +0900

    build : 테스트 환경 더미데이터 sql 및 설정 추가

---
+++ src/main/resources/testData.sql
@@ -0,0 +1,83 @@
+insert into job
+values (1, current_timestamp, current_timestamp, concat('job', 1));
+insert into job
+values (2, current_timestamp, current_timestamp, concat('job', 2));
+insert into job
+values (3, current_timestamp, current_timestamp, concat('job', 3));
+insert into job
+values (4, current_timestamp, current_timestamp, concat('job', 4));
+insert into job
+values (5, current_timestamp, current_timestamp, concat('job', 5));
+insert into job
+values (6, current_timestamp, current_timestamp, concat('job', 6));
+
+insert into user_info
+values (1, current_timestamp, current_timestamp, 0, concat('email', 1), 'NONE', 'YES', 'YES',
+        concat('name', 1),
+        concat('nickName', 1), concat('phone', 1),
+        'ACTIVE', 'MENTOR', 'NONE', 1);
+insert into user_info
+values (2, current_timestamp, current_timestamp, 0, concat('email', 2), 'NONE', 'YES', 'YES',
+        concat('name', 1),
+        concat('nickName', 1), concat('phone', 1),
+        'ACTIVE', 'MENTOR', 'NONE', 1);
+insert into user_info
+values (3, current_timestamp, current_timestamp, 0, concat('email', 3), 'NONE', 'YES', 'YES',
+        concat('name', 1),
+        concat('nickName', 1), concat('phone', 1),
+        'ACTIVE', 'MENTOR', 'NONE', 1);
+insert into user_info
+values (4, current_timestamp, current_timestamp, 0, concat('email', 4), 'NONE', 'YES', 'YES',
+        concat('name', 1),
+        concat('nickName', 1), concat('phone', 1),
+        'ACTIVE', 'MENTOR', 'NONE', 1);
+insert into user_info
+values (5, current_timestamp, current_timestamp, 0, concat('email', 5), 'NONE', 'YES', 'YES',
+        concat('name', 1),
+        concat('nickName', 1), concat('phone', 1),
+        'ACTIVE', 'MENTOR', 'NONE', 1);
+insert into user_info
+values (6, current_timestamp, current_timestamp, 0, concat('email', 6), 'NONE', 'YES', 'YES',
+        concat('name', 1),
+        concat('nickName', 1), concat('phone', 1),
+        'ACTIVE', 'MENTOR', 'NONE', 1);
+
+insert into cover_letter_category
+values (1, current_timestamp, current_timestamp, concat('coverLetterCategory', 1));
+insert into cover_letter_category
+values (2, current_timestamp, current_timestamp, concat('coverLetterCategory', 2));
+insert into cover_letter_category
+values (3, current_timestamp, current_timestamp, concat('coverLetterCategory', 3));
+insert into cover_letter_category
+values (4, current_timestamp, current_timestamp, concat('coverLetterCategory', 4));
+insert into cover_letter_category
+values (5, current_timestamp, current_timestamp, concat('coverLetterCategory', 5));
+insert into cover_letter_category
+values (6, current_timestamp, current_timestamp, concat('coverLetterCategory', 6));
+
+insert into cover_letter
+values (1, current_timestamp, current_timestamp, concat('content', 1), 1, 'ACTIVE', 'WRITING', 1, 1);
+insert into cover_letter
+values (2, current_timestamp, current_timestamp, concat('content', 2), 1, 'ACTIVE', 'WRITING', 1, 2);
+insert into cover_letter
+values (3, current_timestamp, current_timestamp, concat('content', 3), 1, 'ACTIVE', 'WRITING', 1, 3);
+insert into cover_letter
+values (4, current_timestamp, current_timestamp, concat('content', 4), 1, 'ACTIVE', 'WRITING', 1, 4);
+insert into cover_letter
+values (5, current_timestamp, current_timestamp, concat('content', 5), 1, 'ACTIVE', 'WRITING', 1, 5);
+insert into cover_letter
+values (6, current_timestamp, current_timestamp, concat('content', 6), 1, 'ACTIVE', 'WRITING', 1, 6);
+insert into cover_letter
+values (7, current_timestamp, current_timestamp, concat('content', 7), 1, 'ACTIVE', 'WRITING', 1, 1);
+insert into cover_letter
+values (8, current_timestamp, current_timestamp, concat('content', 8), 1, 'ACTIVE', 'WRITING', 1, 2);
+insert into cover_letter
+values (9, current_timestamp, current_timestamp, concat('content', 9), 1, 'ACTIVE', 'WRITING', 1, 3);
+insert into cover_letter
+values (10, current_timestamp, current_timestamp, concat('content', 10), 1, 'ACTIVE', 'WRITING', 1, 4);
+insert into cover_letter
+values (11, current_timestamp, current_timestamp, concat('content', 11), 1, 'ACTIVE', 'WRITING', 1, 5);
+insert into cover_letter
+values (12, current_timestamp, current_timestamp, concat('content', 12), 1, 'ACTIVE', 'WRITING', 1, 6);
+insert into cover_letter
+values (13, current_timestamp, current_timestamp, concat('content', 13), 1, 'ACTIVE', 'WRITING', 1, 1);
