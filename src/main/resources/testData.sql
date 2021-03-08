7dfe338358ce64c6be2d0a0ed643bd9bd6066671
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-08 20:55:42 +0900

    build : testData.sql -> 코멘트 더미데이터 추가

--- src/main/resources/testData.sql
+++ src/main/resources/testData.sql
@@ -81,3 +81,16 @@ insert into cover_letter
 values (12, current_timestamp, current_timestamp, concat('content', 12), 1, 'ACTIVE', 'WRITING', 1, 6);
 insert into cover_letter
 values (13, current_timestamp, current_timestamp, concat('content', 13), 1, 'ACTIVE', 'WRITING', 1, 1);
+
+insert into comment
+values (1, current_timestamp, current_timestamp, 'NONE', 'NONE', concat('comment', 1), 'YES', 'NONE', 'NONE', 'ACTIVE',
+        1, 1);
+insert into comment
+values (2, current_timestamp, current_timestamp, 'NONE', 'NONE', concat('comment', 1), 'YES', 'NONE', 'NONE', 'ACTIVE',
+        2, 1);
+insert into comment
+values (3, current_timestamp, current_timestamp, 'NONE', 'NONE', concat('comment', 1), 'YES', 'NONE', 'NONE', 'ACTIVE',
+        3, 1);
+insert into comment
+values (4, current_timestamp, current_timestamp, 'NONE', 'NONE', concat('comment', 1), 'YES', 'NONE', 'NONE', 'ACTIVE',
+        4, 1);
