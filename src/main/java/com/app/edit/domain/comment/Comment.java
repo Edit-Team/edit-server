7f7beff62241cfc23192efa309d61405f183be06
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 04:24:49 +0900

    feat : Comment -> GetCommentsRes 변환 메서드 추가

--- src/main/java/com/app/edit/domain/comment/Comment.java
+++ src/main/java/com/app/edit/domain/comment/Comment.java
@@ -7,6 +7,7 @@ import com.app.edit.domain.coverletter.CoverLetter;
 import com.app.edit.domain.user.UserInfo;
 import com.app.edit.enums.IsAdopted;
 import com.app.edit.enums.State;
+import com.app.edit.response.comment.GetCommentsRes;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -125,5 +126,19 @@ public class Comment extends BaseEntity {
         this.state = state;
         this.commentDeclarations = commentDeclarations;
         this.appreciates = appreciates;
+    }
+
+    public GetCommentsRes toGetCommentsRes() {
+        Long commentId = this.id;
+        String nickName = this.userInfo.getNickName();
+        String jobName = this.userInfo.getJob().getName();
+        String sentenceEvaluation = this.sentenceEvaluation;
+        String concretenessLogic = this.concretenessLogic;
+        String sincerity = this.sincerity;
+        String activity = this.activity;
+        String commentContent = this.content;
+        IsAdopted isAdopted = this.isAdopted;
+        return new GetCommentsRes(commentId, nickName, jobName, sentenceEvaluation, concretenessLogic,
+                sincerity, activity, commentContent, isAdopted);
     }
 }
