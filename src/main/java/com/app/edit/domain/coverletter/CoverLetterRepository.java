030fde2ad6bccce6eb2ba6b6d36095c223ed7dc6
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-09 17:34:56 +0900

    feat : 많은 분들이 공감하고 있어요 조회 쿼리 추가

--- src/main/java/com/app/edit/domain/coverletter/CoverLetterRepository.java
+++ src/main/java/com/app/edit/domain/coverletter/CoverLetterRepository.java
@@ -1,6 +1,8 @@
 package com.app.edit.domain.coverletter;
 
 import com.app.edit.enums.IsAdopted;
+import com.app.edit.enums.State;
+import com.app.edit.response.coverletter.GetCoverLettersRes;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +11,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface CoverLetterRepository extends JpaRepository<CoverLetter, Long> {
@@ -30,5 +33,15 @@ public interface CoverLetterRepository extends JpaRepository<CoverLetter, Long> 
      * 채택이 완료되었어요 조회 쿼리
      **/
     @Query(value = "select cl from CoverLetter cl where exists(select c from Comment c where c.isAdopted = :isAdopted and c.coverLetter = cl)")
-    Page<CoverLetter> findCoverLettersHasAdoptedComment(Pageable pageable,@Param("isAdopted") IsAdopted isAdopted);
+    Page<CoverLetter> findCoverLettersHasAdoptedComment(Pageable pageable, @Param("isAdopted") IsAdopted isAdopted);
+
+    /*
+     * 많은 분들이 공감하고 있어요 조회 쿼리
+     **/
+    @Query(value = "select * " +
+            "from cover_letter cl " +
+            "where cl.created_At >= :beforeThreeDays " +
+            "order by (select count(*) from sympathy s where s.cover_letter_id = cl.id and s.state = :state) desc ",
+            nativeQuery = true)
+    Page<CoverLetter> findCoverLettersHasManySympathies(Pageable pageable, @Param("beforeThreeDays") LocalDateTime beforeThreeDays, @Param("state") String state);
 }
