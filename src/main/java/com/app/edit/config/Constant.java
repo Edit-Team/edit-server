a9507383bfc9f12ac83d84849dd4204b84d7f9d3
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-09 17:27:59 +0900

    feat : 상수 추가

--- src/main/java/com/app/edit/config/Constant.java
+++ src/main/java/com/app/edit/config/Constant.java
@@ -3,5 +3,6 @@ package com.app.edit.config;
 public class Constant {
     public static int ZERO = 0;
     public static int ONE = 1;
+    public static int CAN_STAY_DAY = 3;
     public static int DEFAULT_PAGE_SIZE = 10;
 }
