6e8169a469a64f8b0884278db62e5c731e8f30c9
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 06:11:50 +0900

    feat : 코멘트 신고 요청 DTO -> PostCommentDeclarationReq 추가

---
+++ src/main/java/com/app/edit/request/comment/PostDeclareCommentReq.java
@@ -0,0 +1,13 @@
+package com.app.edit.request.comment;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostDeclareCommentReq {
+
+    private Long commentId;
+}
