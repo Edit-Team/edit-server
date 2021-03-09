53a455245d6f9512aac5ac0512e872907399285a
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-09 23:29:47 +0900

    feat : 상수 추가

--- src/main/java/com/app/edit/config/Constant.java
+++ src/main/java/com/app/edit/config/Constant.java
@@ -5,4 +5,6 @@ public class Constant {
     public static int ONE = 1;
     public static int CAN_STAY_DAY = 3;
     public static int DEFAULT_PAGE_SIZE = 10;
+    public static boolean DEFAULT_SYMPATHY = false;
+    public static Long DEFAULT_SYMPATHIES_COUNT = 0L;
 }
