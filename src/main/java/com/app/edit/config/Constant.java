89cdaad7fb0c39a7439120d5c492d653043687e8
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 03:08:18 +0900

    feat : 메인 화면에 사용되는 상수 추가

--- src/main/java/com/app/edit/config/Constant.java
+++ src/main/java/com/app/edit/config/Constant.java
@@ -7,4 +7,6 @@ public class Constant {
     public static int DEFAULT_PAGE_SIZE = 10;
     public static boolean DEFAULT_SYMPATHY = false;
     public static Long DEFAULT_SYMPATHIES_COUNT = 0L;
+    public static int MAIN_TODAY_COVER_LETTERS_COUNT = 5;
+    public static int MAIN_ANOTHER_COVER_LETTERS_COUNT = 2;
 }
