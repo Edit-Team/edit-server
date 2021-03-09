e1716e2975f668f9eb11cd942f13b5e894134f12
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 05:31:20 +0900

    feat : 코멘트 조회 메서드 추가

--- src/main/java/com/app/edit/provider/CommentProvider.java
+++ src/main/java/com/app/edit/provider/CommentProvider.java
@@ -1,18 +1,35 @@
 package com.app.edit.provider;
 
+import com.app.edit.config.BaseException;
 import com.app.edit.domain.comment.CommentRepository;
+import com.app.edit.domain.coverletter.CoverLetter;
+import com.app.edit.response.comment.GetCommentsRes;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Transactional(readOnly = true)
 @Service
 public class CommentProvider {
 
     private final CommentRepository commentRepository;
+    private final CoverLetterProvider coverLetterProvider;
 
     @Autowired
-    public CommentProvider(CommentRepository commentRepository) {
+    public CommentProvider(CommentRepository commentRepository, CoverLetterProvider coverLetterProvider) {
         this.commentRepository = commentRepository;
+        this.coverLetterProvider = coverLetterProvider;
+    }
+
+    public List<GetCommentsRes> retrieveCommentsByCoverLetterId(Pageable pageable, Long coverLetterId) throws BaseException {
+        CoverLetter coverLetter = coverLetterProvider.getCoverLetterById(coverLetterId);
+        return commentRepository.findCommentsByCoverLetter(pageable, coverLetter).stream()
+                .map(comment -> comment.toGetCommentsRes())
+                .collect(toList());
     }
 }
