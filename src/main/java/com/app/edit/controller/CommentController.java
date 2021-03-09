ac7f798257658b59fbd14e60028b28f9be73e461
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 05:31:43 +0900

    feat : 자소서에 달린 코멘트 조회 API 추가

--- src/main/java/com/app/edit/controller/CommentController.java
+++ src/main/java/com/app/edit/controller/CommentController.java
@@ -1,9 +1,17 @@
 package com.app.edit.controller;
 
+import com.app.edit.config.BaseException;
+import com.app.edit.config.BaseResponse;
+import com.app.edit.config.BaseResponseStatus;
 import com.app.edit.provider.CommentProvider;
+import com.app.edit.response.comment.GetCommentsRes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.app.edit.config.Constant.DEFAULT_PAGE_SIZE;
 
 @RequestMapping("/api")
 @RestController
@@ -14,5 +22,16 @@ public class CommentController {
     @Autowired
     public CommentController(CommentProvider commentProvider) {
         this.commentProvider = commentProvider;
+    }
+
+    /*
+     * 자소서에 달린 코멘트 조회 API
+     **/
+    @GetMapping("/cover-letters/{cover-letter-id}/comments")
+    public BaseResponse<List<GetCommentsRes>> getComments(@PathVariable("cover-letter-id") Long coverLetterId,
+                                                          @RequestParam Integer page) throws BaseException {
+        PageRequest pageRequest = com.app.edit.config.PageRequest.of(page, DEFAULT_PAGE_SIZE);
+        return new BaseResponse<>(BaseResponseStatus.SUCCESS,
+                commentProvider.retrieveCommentsByCoverLetterId(pageRequest, coverLetterId));
     }
 }
