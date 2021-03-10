6811b414fa5064dcba1245400a50ff0ddb7be771
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 21:37:50 +0900

    feat : 완성중인 자소서 등록 메서드 추가

--- src/main/java/com/app/edit/service/CoverLetterService.java
+++ src/main/java/com/app/edit/service/CoverLetterService.java
@@ -4,11 +4,14 @@ import com.app.edit.config.BaseException;
 import com.app.edit.domain.coverletter.CoverLetter;
 import com.app.edit.domain.coverletter.CoverLetterRepository;
 import com.app.edit.domain.coverlettercategory.CoverLetterCategory;
+import com.app.edit.domain.temporarycoverletter.TemporaryCoverLetter;
 import com.app.edit.domain.user.UserInfo;
 import com.app.edit.enums.CoverLetterType;
 import com.app.edit.enums.State;
 import com.app.edit.provider.CoverLetterCategoryProvider;
+import com.app.edit.provider.CoverLetterProvider;
 import com.app.edit.provider.UserInfoProvider;
+import com.app.edit.request.coverletter.PostCompletingCoverLetterReq;
 import com.app.edit.request.coverletter.PostWritingCoverLetterReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,12 +25,16 @@ public class CoverLetterService {
 
     private final CoverLetterRepository coverLetterRepository;
     private final UserInfoProvider userInfoProvider;
+    private final CoverLetterProvider coverLetterProvider;
     private final CoverLetterCategoryProvider coverLetterCategoryProvider;
 
     @Autowired
-    public CoverLetterService(CoverLetterRepository coverLetterRepository, UserInfoProvider userInfoProvider, CoverLetterCategoryProvider coverLetterCategoryProvider) {
+    public CoverLetterService(CoverLetterRepository coverLetterRepository, UserInfoProvider userInfoProvider,
+                              CoverLetterProvider coverLetterProvider,
+                              CoverLetterCategoryProvider coverLetterCategoryProvider) {
         this.coverLetterRepository = coverLetterRepository;
         this.userInfoProvider = userInfoProvider;
+        this.coverLetterProvider = coverLetterProvider;
         this.coverLetterCategoryProvider = coverLetterCategoryProvider;
     }
 
@@ -47,5 +54,24 @@ public class CoverLetterService {
         userInfo.addCoverLetter(requestedCoverLetter);
         CoverLetter savedCoverLetter = coverLetterRepository.save(requestedCoverLetter);
         return savedCoverLetter.getId();
+    }
+
+    public Long createCompletingCoverLetter(PostCompletingCoverLetterReq request) throws BaseException {
+        Long userInfoId = 1L;
+        Long originalCoverLetterId = request.getOriginalCoverLetterId();
+        String content = request.getCoverLetterContent();
+        CoverLetter originalCoverLetter = coverLetterProvider.getCoverLetterById(originalCoverLetterId);
+        CoverLetterCategory originalCoverLetterCategory = originalCoverLetter.getCoverLetterCategory();
+        UserInfo userInfo = userInfoProvider.getUserInfoById(userInfoId);
+        CoverLetter requestedCompletingCoverLetter = CoverLetter.builder()
+                .coverLetterCategory(originalCoverLetterCategory)
+                .originalCoverLetterId(originalCoverLetterId)
+                .content(content)
+                .state(State.ACTIVE)
+                .type(CoverLetterType.COMPLETING)
+                .build();
+        userInfo.addCoverLetter(requestedCompletingCoverLetter);
+        CoverLetter savedTemporaryCoverLetter = coverLetterRepository.save(requestedCompletingCoverLetter);
+        return savedTemporaryCoverLetter.getId();
     }
 }
