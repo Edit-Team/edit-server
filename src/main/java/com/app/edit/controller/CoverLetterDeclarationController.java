a6cc1d1ab1ed5020a75ca66798d5a95dd26bdbb5
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 17:29:40 +0900

    feat : 자소서 신고 API 추가

--- src/main/java/com/app/edit/controller/CoverLetterDeclarationController.java
+++ src/main/java/com/app/edit/controller/CoverLetterDeclarationController.java
@@ -1,7 +1,13 @@
 package com.app.edit.controller;
 
+import com.app.edit.config.BaseException;
+import com.app.edit.config.BaseResponse;
+import com.app.edit.config.BaseResponseStatus;
+import com.app.edit.request.coverletter.PostCoverLetterDeclarationReq;
 import com.app.edit.service.CoverLetterDeclarationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +20,14 @@ public class CoverLetterDeclarationController {
     @Autowired
     public CoverLetterDeclarationController(CoverLetterDeclarationService coverLetterDeclarationService) {
         this.coverLetterDeclarationService = coverLetterDeclarationService;
+    }
+
+    /*
+     * 자소서 신고 API
+     **/
+    @PostMapping("/declare-cover-letters")
+    public BaseResponse<Long> postCoverLetterDeclaration(@RequestBody PostCoverLetterDeclarationReq request) throws BaseException {
+        return new BaseResponse<>(BaseResponseStatus.SUCCESS,
+                coverLetterDeclarationService.createCoverLetterDeclaration(request));
     }
 }
