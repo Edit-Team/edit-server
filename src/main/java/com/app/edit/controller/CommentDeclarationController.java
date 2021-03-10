3acff654d5852107b05252bc94ce0982381b7d04
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 17:11:54 +0900

    feat : 코멘트 신고 API 추가

--- src/main/java/com/app/edit/controller/CommentDeclarationController.java
+++ src/main/java/com/app/edit/controller/CommentDeclarationController.java
@@ -1,7 +1,13 @@
 package com.app.edit.controller;
 
+import com.app.edit.config.BaseException;
+import com.app.edit.config.BaseResponse;
+import com.app.edit.config.BaseResponseStatus;
+import com.app.edit.request.comment.PostDeclareCommentReq;
 import com.app.edit.service.CommentDeclarationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +20,13 @@ public class CommentDeclarationController {
     @Autowired
     public CommentDeclarationController(CommentDeclarationService commentDeclarationService) {
         this.commentDeclarationService = commentDeclarationService;
+    }
+
+    /*
+     * 코멘트 신고 API
+     **/
+    @PostMapping("/declare-comments")
+    public BaseResponse<Long> postDeclareComment(@RequestBody PostDeclareCommentReq request) throws BaseException {
+        return new BaseResponse<>(BaseResponseStatus.SUCCESS, commentDeclarationService.createCommentDeclaration(request));
     }
 }
