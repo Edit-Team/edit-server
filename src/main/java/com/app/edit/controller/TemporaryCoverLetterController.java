1d5513a5ff38c9bc533b2414116f64a8f6cc9fe7
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 19:02:31 +0900

    feat : 작성중인 자소서 등록 API 추가

--- src/main/java/com/app/edit/controller/TemporaryCoverLetterController.java
+++ src/main/java/com/app/edit/controller/TemporaryCoverLetterController.java
@@ -1,7 +1,13 @@
 package com.app.edit.controller;
 
+import com.app.edit.config.BaseException;
+import com.app.edit.config.BaseResponse;
+import com.app.edit.config.BaseResponseStatus;
+import com.app.edit.request.temporarycoverletter.PostTemporaryCoverLetterReq;
 import com.app.edit.service.TemporaryCoverLetterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +20,14 @@ public class TemporaryCoverLetterController {
     @Autowired
     public TemporaryCoverLetterController(TemporaryCoverLetterService temporaryCoverLetterService) {
         this.temporaryCoverLetterService = temporaryCoverLetterService;
+    }
+
+    /*
+     * 작성중인 자소서 등록 API
+     **/
+    @PostMapping("/writing-temporary-cover-letters")
+    public BaseResponse<Long> postTemporaryCoverLetters(@RequestBody PostTemporaryCoverLetterReq request) throws BaseException {
+        return new BaseResponse<>(BaseResponseStatus.SUCCESS,
+                temporaryCoverLetterService.createWritingTemporaryCoverLetter(request));
     }
 }
