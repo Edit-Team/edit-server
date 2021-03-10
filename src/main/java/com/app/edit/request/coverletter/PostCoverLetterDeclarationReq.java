2f028590b29245a030328db2fa6d8a9f763a84bc
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-10 17:26:29 +0900

    feat : 자소서 신고 요청 DTO -> PostCoverLetterDeclarationReq 추가

---
+++ src/main/java/com/app/edit/request/coverletter/PostCoverLetterDeclarationReq.java
@@ -0,0 +1,13 @@
+package com.app.edit.request.coverletter;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class PostCoverLetterDeclarationReq {
+
+    private Long coverLetterId;
+}
