a34822b6745c2b55254e13d968c30bf5dc2e0e8b
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-15 17:58:19 +0900

    feat : CommentService 추가

---
+++ src/main/java/com/app/edit/service/CommentService.java
@@ -0,0 +1,18 @@
+package com.app.edit.service;
+
+import com.app.edit.domain.comment.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+}
