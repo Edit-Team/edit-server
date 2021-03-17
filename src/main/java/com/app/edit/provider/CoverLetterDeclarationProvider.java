4047a34ac35df41b3c34f8769857c592fbeef8b3
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-17 15:24:12 +0900

    feat : 자소서 신고 조회 기능 및 예외 처리 추가

--- src/main/java/com/app/edit/provider/CoverLetterDeclarationProvider.java
+++ src/main/java/com/app/edit/provider/CoverLetterDeclarationProvider.java
@@ -1,9 +1,15 @@
 package com.app.edit.provider;
 
+import com.app.edit.config.BaseException;
+import com.app.edit.config.BaseResponseStatus;
+import com.app.edit.domain.coverletterdeclaration.CoverLetterDeclaration;
 import com.app.edit.domain.coverletterdeclaration.CoverLetterDeclarationRepository;
+import com.app.edit.enums.IsProcessing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 @Service
@@ -14,5 +20,17 @@ public class CoverLetterDeclarationProvider {
     @Autowired
     public CoverLetterDeclarationProvider(CoverLetterDeclarationRepository coverLetterDeclarationRepository) {
         this.coverLetterDeclarationRepository = coverLetterDeclarationRepository;
+    }
+
+    public CoverLetterDeclaration getCoverLetterDeclarationById(Long coverLetterDeclarationId) throws BaseException {
+        Optional<CoverLetterDeclaration> coverLetterDeclaration = coverLetterDeclarationRepository
+                .findById(coverLetterDeclarationId);
+        if (coverLetterDeclaration.isEmpty()) {
+            throw new BaseException(BaseResponseStatus.NOT_FOUND_COVER_LETTER_DECLARATION);
+        }
+        if (coverLetterDeclaration.get().getIsProcessing().equals(IsProcessing.YES)) {
+            throw new BaseException(BaseResponseStatus.ALREADY_PROCESSED_COVER_LETTER_DECLARATION);
+        }
+        return coverLetterDeclaration.get();
     }
 }
