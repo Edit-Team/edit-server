48cce26f676b48c8c072df9ed76430cd37c913a1
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-29 19:18:40 +0900

    refactor : 감사 여부 조회 쿼리 수정

--- src/main/java/com/app/edit/domain/appreciate/AppreciateRepository.java
+++ src/main/java/com/app/edit/domain/appreciate/AppreciateRepository.java
@@ -1,9 +1,11 @@
 package com.app.edit.domain.appreciate;
 
+import com.app.edit.domain.comment.Comment;
 import com.app.edit.domain.user.UserInfo;
 import com.app.edit.enums.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +13,6 @@ import java.util.Optional;
 @Repository
 public interface AppreciateRepository extends JpaRepository<Appreciate, AppreciateId> {
 
-    Optional<Appreciate> findByUserInfoAndState(UserInfo userInfo, State state);
+    @Query(value = "select a from Appreciate a where a.userInfo.id = :userInfoId and a.comment = :comment and a.state = :state")
+    Optional<Appreciate> findByUserInfoAndCommentAndState(@Param("userInfoId") Long userInfoId, @Param("comment") Comment comment, @Param("state") State state);
 }
