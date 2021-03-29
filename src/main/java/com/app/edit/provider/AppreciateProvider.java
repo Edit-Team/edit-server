483be8bc1d5930db8e92f9c7b095455d58347d86
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-29 18:41:55 +0900

    feat : 감사 여부 조회 기능 추가

--- src/main/java/com/app/edit/provider/AppreciateProvider.java
+++ src/main/java/com/app/edit/provider/AppreciateProvider.java
@@ -1,9 +1,15 @@
 package com.app.edit.provider;
 
+import com.app.edit.domain.appreciate.Appreciate;
 import com.app.edit.domain.appreciate.AppreciateRepository;
+import com.app.edit.domain.comment.Comment;
+import com.app.edit.domain.user.UserInfo;
+import com.app.edit.enums.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 @Service
@@ -14,5 +20,10 @@ public class AppreciateProvider {
     @Autowired
     public AppreciateProvider(AppreciateRepository appreciateRepository) {
         this.appreciateRepository = appreciateRepository;
+    }
+
+    public Optional<Appreciate> getAppreciateByComment(Comment comment) {
+        UserInfo userInfo = comment.getUserInfo();
+        return appreciateRepository.findByUserInfoAndState(userInfo, State.ACTIVE);
     }
 }
