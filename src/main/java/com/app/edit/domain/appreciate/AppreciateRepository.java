626ecb7d8b676f7c9846e90c01142f7129497857
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-29 18:41:11 +0900

    feat : 감사 여부 조회 쿼리 추가

--- src/main/java/com/app/edit/domain/appreciate/AppreciateRepository.java
+++ src/main/java/com/app/edit/domain/appreciate/AppreciateRepository.java
@@ -1,8 +1,15 @@
 package com.app.edit.domain.appreciate;
 
+import com.app.edit.domain.user.UserInfo;
+import com.app.edit.enums.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AppreciateRepository extends JpaRepository<Appreciate, AppreciateId> {
+
+    Optional<Appreciate> findByUserInfoAndState(UserInfo userInfo, State state);
 }
